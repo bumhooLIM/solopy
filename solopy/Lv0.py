3cf83f1d5a666a50ec39e1140f72d1ffb7492b08
bumhooLIM <bumhoo7@snu.ac.kr>
2025-09-25 17:54:02 +0900

    execute lv0 example

--- solopy/Lv0.py
+++ solopy/Lv0.py
@@ -28,109 +28,36 @@ class Lv0:
             file_h.setFormatter(handler.formatter)
             self.logger.addHandler(file_h)
 
+    # --- helpers ---
     def _is_bz2(self, path: Path) -> bool:
-        # True for .../file.fits.bz2 (or any *.bz2)
         return ".bz2" in "".join(path.suffixes)
 
     def _open_fits_any(self, path: Path):
-        """Open .fits or .fits.bz2 and return (hdul, was_bz2_bool)."""
+        """
+        Open .fits or .fits.bz2.
+
+        Returns
+        -------
+        hdul : fits.HDUList
+        was_bz2 : bool
+        fobj : file-like or None   # keep this open until after reading data
+        """
         if self._is_bz2(path):
-            with bz2.open(path, "rb") as fobj:
-                hdul = fits.open(fobj, memmap=False)
-            return hdul, True
+            fobj = bz2.BZ2File(path, "rb")
+            hdul = fits.open(fobj, memmap=False)
+            return hdul, True, fobj
         else:
             hdul = fits.open(path, memmap=False)
-            return hdul, False
-        
+            return hdul, False, None
+
     def _write_fits_any(self, path: Path, hdu: fits.PrimaryHDU, as_bz2: bool):
-        """Write PrimaryHDU to .fits or .fits.bz2."""
         if as_bz2:
-            # Write FITS bytes to memory, then bzip2-compress to disk
             buf = io.BytesIO()
             hdu.writeto(buf, overwrite=True)
-            data = bz2.compress(buf.getvalue())
-            path.parent.mkdir(parents=True, exist_ok=True)
             with open(path, "wb") as f:
-                f.write(data)
+                f.write(bz2.compress(buf.getvalue()))
         else:
             hdu.writeto(path, overwrite=True)
-
-    # def update_header(self, fpath_fits):
-    #     """
-    #     Validate and update FITS headers.
-
-    #     Parameters
-    #     ----------
-    #     fpath_fits : str or pathlib.Path
-    #         Path to the Level-0 FITS file to update.
-    #     """
-    #     fpath = Path(fpath_fits)
-    #     if not fpath.exists():
-    #         self.logger.error(f"File not found: {fpath}")
-    #         return
-
-    #     hdul = fits.open(fpath)
-    #     hdr = hdul[0].header.copy()
-
-    #     ### Core metadata fields
-    #     hdr.comments['LT']  = 'Local Time'
-    #     # hdr.comments['UTC'] = 'Universal Time Coordinated'
-    #     hdr['EXPTIME']      = (float(hdr['EXPTIME']), 'Exposure Time (sec)')
-    #     hdr['CCDTEMP']      = (float(hdr['CCDTEMP']), 'CCD Temperature (C)')
-    #     hdr['PIXSZ']        = (float(hdr['PIXSZ']), 'Pixel Size (um)')
-    #     hdr['JD']           = (float(hdr['JD']), 'Julian Date')
-    #     hdr['DATE-OBS']     = (Time(hdr['JD'], format='jd').isot, 'Observation Datetime')
-    #     hdr['MJD-OBS']      = (Time(hdr['JD'], format='jd').mjd, 'Modified Julian Date')
-    #     hdr['FOCALLEN']     = (float(hdr['FOCALLEN']), 'Focal Length (mm)')
-    #     hdr['APTDIA']       = (float(hdr['APTDIA']), 'Aperture Diameter (mm)')
-    #     hdr['FOCUS']        = (int(hdr['FOCUS']), 'Focal Position')
-    #     hdr['OBSERVER']     = (hdr['OBSERVER'].upper(), 'Observer')
-    #     hdr['IMAGETYP']     = (hdr['IMAGETYP'].upper(), 'Image Type')
-
-    #     ### Telescope & Site
-    #     for key, unit, comment in [('RA', 'hourangle', 'Telescope RA (deg)'),
-    #                                ('DEC', 'degree', 'Telescope Dec (deg)'),
-    #                                ('ALT', 'degree', 'Telescope Alt (deg)'),
-    #                                ('AZ', 'degree', 'Telescope Az (deg)')]:
-    #         if isinstance(hdr[key], str):
-    #             hdr[key] = (Angle(hdr[key], unit=unit).degree, comment)
-    #     hdr['LON']   = (-119.4, 'Site Longitude (deg)')
-    #     hdr['LAT']   = (37.07, 'Site Latitude (deg)')
-    #     hdr['ELEV']  = (1.405, 'Site Elevation (km)')
-
-    #     ### Processing flags
-    #     flags = ['BIASCORR', 'DARKCORR', 'FLATCORR']
-    #     for flag in flags:
-    #         hdr[flag] = (False, f'{flag.capitalize()} applied?')
-    #     hdr['DATLEVEL']  = (0, 'Data Process Level')
-    #     hdr['COMBINED']  = (False, 'Combined frames?')
-
-    #     ### Additional coordinates
-    #     obstime = Time(hdr['JD'], format='jd')
-    #     coords = SkyCoord(ra=hdr['RA']*u.deg, dec=hdr['DEC']*u.deg, frame='icrs')
-    #     gcrs = coords.transform_to(GCRS(obstime=obstime))
-    #     gal  = coords.galactic
-    #     ecl  = coords.transform_to(GeocentricTrueEcliptic(equinox=obstime))
-
-    #     # Solar & lunar elongation
-    #     sun_gcrs  = get_sun(obstime)
-    #     moon_gcrs = get_body('Moon', obstime)
-    #     hdr['SELONG'] = (gcrs.separation(sun_gcrs).value, 'Solar elongation (deg)')
-    #     hdr['MELONG'] = (gcrs.separation(moon_gcrs).value, 'Lunar elongation (deg)')
-
-    #     # Galactic & ecliptic coords
-    #     hdr['GXLAT'] = (gal.b.value, 'Galactic latitude (deg)')
-    #     hdr['GXLON'] = (gal.l.value, 'Galactic longitude (deg)')
-    #     hdr['ECLAT'] = (ecl.lat.value, 'Ecliptic latitude (deg)')
-    #     hdr['ECLON'] = (ecl.lon.value, 'Ecliptic longitude (deg)')
-
-    #     hdr['HISTORY'] = f"({datetime.now().isoformat()}) Lv0 header updated."
-
-    #     ### Write back
-    #     new_hdu = fits.PrimaryHDU(data=hdul[0].data, header=hdr)
-    #     new_hdu.writeto(fpath, overwrite=True)
-    #     self.logger.info(f"Updated Lv0 header: {fpath}")
-
     
     def update_header(self, fpath_fits):
         """
@@ -146,9 +73,21 @@ class Lv0:
             self.logger.error(f"File not found: {fpath}")
             return
 
-        hdul, was_bz2 = self._open_fits_any(fpath)
-        hdr = hdul[0].header.copy()
-        data = hdul[0].data
+        hdul, was_bz2, fobj = self._open_fits_any(fpath)
+        try:  
+            hdr = hdul[0].header.copy()
+            data = hdul[0].data.copy()
+        finally:
+
+            try:
+                hdul.close()
+            finally:
+                if fobj is not None:
+                    fobj.close()
+
+        # Remove all HISTORY cards if present
+        while 'HISTORY' in hdr:
+            hdr.remove('HISTORY')
 
         # --- Core metadata
         hdr.comments['LT']  = 'Local Time'
